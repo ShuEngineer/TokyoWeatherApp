2f2e6e6d3b05ec9af67847d706ff6b3e18bf390c
ShuEngineer <supercar110shu@gmail.com>
2020-02-10 00:11:25 +0900

    2020/02/09　スプラッシュ画面の追加

--- TokyoWeatherApp/ViewController.swift
+++ TokyoWeatherApp/ViewController.swift
@@ -7,14 +7,118 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+    @IBOutlet weak var tableView: UITableView!
+    
+    var weatherDayTimes = [String?]()   //天気情報：日時
+    
+    var weatherDetails = [String?]()    //天気情報：天気詳細
+    
+    var weatherGroups = [String?]()     //天気情報：天気グループ
+    
+    var weatherInfos = [String?]()      //一列分の天気情報
+    
+    var weatherGroupInfo = [String?]()  //天気情報の配列
+    
+    var weatherTemp = [Double?]()       //天気情報：気温
+    let cellNum = 40                    //JSON情報の配列数
+    
+    private let refreshControl = UIRefreshControl()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "Tokyo🗼Weather"         // Navigation Barのタイトルを設定
+        tableView.delegate = self
+        tableView.dataSource = self    // dataSouceプロパティに自身(WeatherListViewController)を代入
+        
+        getWeatherInfo()
+        
+        tableView.refreshControl = refreshControl
+        refreshControl.addTarget(self, action: #selector(ViewController.refresh(sender:)), for: .valueChanged)
     }
-
-
+    
+    //WeatherDetailsViewに遷移する準備
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let weatherDetailsView: WeatherDetailsView = segue.destination as! WeatherDetailsView
+        if segue.identifier == "cellSegue" {
+            let indexPath = self.tableView.indexPathForSelectedRow
+            weatherDetailsView.weather = weatherInfos[indexPath!.row]
+            weatherDetailsView.weatherMain = weatherGroups[indexPath!.row]
+            weatherDetailsView.temp = weatherTemp[indexPath!.row]
+        }
+        
+        
+    }
+    
+    
+    func getWeatherInfo(){
+        
+        //APPIDに個人のKeyを設定
+        let urlString = "https://api.openweathermap.org/data/2.5/forecast?id=524901&q=Tokyo&APPID=9afd2ee30401d11d026bbc1aad91fbdd"
+        
+        Alamofire.request(urlString, method: .get).responseJSON{ response in
+            guard let object = response.result.value else{
+                return
+            }
+            
+            let json = JSON(object)
+            json.forEach{ (key, json) in
+                //天気情報の取得
+                for i in 0...self.cellNum {
+                    let weatherInfoLib1: String? = json[i]["dt_txt"].string
+                    let weatherInfoLib2: String? = json[i]["weather"][0]["description"].string
+                    let weatherInfoLib3: String? = json[i]["weather"][0]["main"].string
+                    let weatherInfoLib4: Double? = json[i]["main"]["temp"].double //気温
+                    
+                    
+                    if weatherInfoLib1 == nil && weatherInfoLib2 == nil && weatherInfoLib3 == nil
+                        && weatherInfoLib4 == nil{
+                        return
+                    }
+                    self.weatherDayTimes.append(weatherInfoLib1)
+                    self.weatherDetails.append(weatherInfoLib2)
+                    self.weatherInfos.append("\(self.weatherDayTimes[i]!) : \(self.weatherDetails[i]!)")
+                    self.weatherGroups.append(weatherInfoLib3)
+                    self.weatherTemp.append(weatherInfoLib4)
+                }
+                
+            }
+            self.tableView.reloadData()
+        }
+    }
+    // データの数（＝セルの数）を返すメソッド
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.weatherInfos.count
+    }
+    
+    // 各セルの内容を返すメソッド
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // 再利用可能な cell を得る
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = self.weatherInfos[indexPath.row]
+        
+        return cell
+    }
+    
+    // 各セルを選択した時に実行されるメソッド
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "cellSegue",sender: nil)
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    //Refresh
+    @objc func refresh(sender: UIRefreshControl) {
+        getWeatherInfo()
+        self.refreshControl.endRefreshing()
+    }
+    
 }
+
 
---
+++ TokyoWeatherApp/WeatherDetailsView.swift
@@ -0,0 +1,101 @@
+//
+//  WeatherDetailsView.swift
+//  TokyoWeatherApp
+//
+//  Created by 伊藤嵩 on 2020/02/09.
+//  Copyright © 2020 Shu Ito. All rights reserved.
+//
+
+import UIKit
+
+class WeatherDetailsView: UIViewController {
+    
+    var weather: String?       //遷移元から天気データを取得
+    var weatherMain: String?   //天気情報のメイングループ
+    var temp: Double?          //遷移元から気温を取得
+    
+    var tempStr: String?
+    
+    @IBOutlet weak var weatherImages: UIImageView!  //天気情報のimage
+    @IBOutlet weak var weatherLabel: UILabel!       //天気情報のラベル
+    
+    @IBOutlet weak var weatherTemp: UILabel!        //天気情報：気温ラベル
+    
+    @IBOutlet weak var tokyoCity: UILabel!          //東京の街
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.weatherLabel!.text = self.weather!
+        
+        weatherLabel.adjustsFontSizeToFitWidth = true
+        tokyoCity.text = "🗼🏢🏢🚶‍♂️🚶‍♀️🏢🏢🌳"
+        tokyoCity.backgroundColor = UIColor.cyan
+        tokyoCity.adjustsFontSizeToFitWidth = true
+        weatherImages.backgroundColor = UIColor.cyan
+        
+        getWeatherImage()
+        
+        self.weatherTemp!.text = "Temperature : \(getWeatherTemp(self.temp!)) ℃"
+        
+    }
+    
+    func getWeatherImage(){
+        
+        switch weatherMain! {
+        case "Thunderstorm":
+            let image:UIImage = UIImage(named: "11d.png")!
+            self.weatherImages.image = image
+            
+        case "Drizzle", "Rain":
+            let image:UIImage = UIImage(named: "09d.png")!
+            self.weatherImages.image = image
+            
+        case "Snow":
+            let image:UIImage = UIImage(named: "13d.png")!
+            self.weatherImages.image = image
+            
+        case "Mist", "Smoke", "Haze", "Dust", "Fog", "Sand", "Ash", "Squall", "Tornado":
+            let image:UIImage = UIImage(named: "50d.png")!
+            self.weatherImages.image = image
+            
+        case "Clear":
+            let image: UIImage = UIImage(named: "01d.png")!
+            self.weatherImages.image = image
+            
+        case "Clouds":
+            let image:UIImage = UIImage(named: "04d.png")!
+            self.weatherImages.image = image
+            
+        default:
+            print("Error:画像取得失敗")
+        }
+        
+        
+        
+    }
+    
+    func getWeatherTemp(_ temp: Double) -> String{
+        
+        var tempResult : Double
+        let absoluteTemperature = 273.15
+        var tempResultStr : String
+        
+        tempResult = (round((temp - absoluteTemperature)*10)/10)
+        tempResultStr = String(tempResult)
+        return tempResultStr
+    }
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
